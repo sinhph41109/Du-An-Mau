32851cd61f717580b4a5b60ef336963d85efa152
sinhph41109 <sinhscph41109@fpt.edu.vn>
2024-12-01 18:30:22 +0700

    7th

--- app/src/main/java/com/example/duanmau/MainActivity.java
+++ app/src/main/java/com/example/duanmau/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.duanmau;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.duanmau.Dao.SachDAO;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,7 +24,20 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        SachDAO dao = new SachDAO(this);
-        dao.getDSDauSach();
+        Toolbar toolbar = findViewById(R.id.toolBar);
+        FrameLayout frameLayout = findViewById(R.id.frameLayout);
+        NavigationView navigationView = findViewById(R.id.navigationView);
+
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if()
+        return super.onOptionsItemSelected(item);
     }
 }
