fbdf3b2f15b92d9d4c9e582aeca820f9e519cd86
sinhph41109 <sinhscph41109@fpt.edu.vn>
2024-12-01 15:18:21 +0700

    4rd commit

--- app/src/main/java/com/example/duanmau/Dao/SachDAO.java
+++ app/src/main/java/com/example/duanmau/Dao/SachDAO.java
@@ -1,12 +1,31 @@
 package com.example.duanmau.Dao;
 
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 
 import com.example.duanmau.DataBase.DBHelper;
+import com.example.duanmau.Model.Sach;
+
+import java.util.ArrayList;
 
 public class SachDAO {
     DBHelper dbHelper;
     public SachDAO(Context context){
         dbHelper = new DBHelper(context);
+    }
+
+    public ArrayList<Sach> getDSDauSach(){
+        ArrayList<Sach> list = new ArrayList<>();
+        SQLiteDatabase sqLiteOpenHelper = dbHelper.getReadableDatabase();
+        Cursor cursor = sqLiteOpenHelper.rawQuery("SELECT * FROM SACH", null);
+        if(cursor.getCount() != 0){
+            cursor.moveToFirst();
+            do{
+                list.add(new Sach(cursor.getInt(0),cursor.getString(1),cursor.getInt(2),cursor.getInt(3)));
+            }while(cursor.moveToNext());
+        }
+        return list;
     }
 }
